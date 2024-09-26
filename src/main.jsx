91567d3bacb348215aa93d4218ed22b994e23a92
Akilraj-1153 <akilrajn1153@gmail.com>
2024-09-27 01:12:45 +0530

    deploying process

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RecoilRoot } from 'recoil'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/BlogNotes">
     <RecoilRoot>
-      <BrowserRouter>
       <App />
-      </BrowserRouter>
     </RecoilRoot>
-)
+  </BrowserRouter>
+);

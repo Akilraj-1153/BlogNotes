29be9fcdc824c923e90b0fd6aac4b2c99b47779b
Akilraj-1153 <akilrajn1153@gmail.com>
2024-09-27 14:20:31 +0530

    netify hosting process

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/BlogNotes">
+  <BrowserRouter >
     <RecoilRoot>
       <App />
     </RecoilRoot>
